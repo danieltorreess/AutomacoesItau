5d853f9e8f4324cc86bcf27c14bb3db13cd28987
Cicero Daniel Torres Da Silva <AB1541240@br.atento.com>
2026-02-18 09:05:38 -0300

    Inclusão de um log melhor no RPA do ServiceNow Alelo...

--- src/Alelo/ServiceNow/app.py
+++ src/Alelo/ServiceNow/app.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from datetime import datetime
 
 from src.Alelo.ServiceNow.email_service import EmailServiceServiceNow
 from src.Alelo.ServiceNow.downloader import salvar_anexo
@@ -11,17 +12,34 @@ DESTINO_REDE = Path(r"\\brsbesrv960\publico\REPORTS\ALELO\SERVICENOW")
 
 def main():
 
-    print("🚀 Iniciando RPA ServiceNow")
+    print("\n🚀 Iniciando RPA ServiceNow")
+    print("===================================================")
 
     email_service = EmailServiceServiceNow()
 
+    print("🔎 Buscando e-mail no Outlook...")
     email = email_service.buscar_ultimo_email(ASSUNTO)
 
     if not email:
         print("⚠️ Nenhum e-mail encontrado.")
         return
 
-    print(f"📨 E-mail encontrado: {email.Subject}")
+    recebido_em = email.ReceivedTime
+    remetente = email.SenderName
+    assunto = email.Subject
+    agora = datetime.now(recebido_em.tzinfo)
+
+    diferenca = (agora - recebido_em).days
+
+    print("📨 E-mail encontrado!")
+    print(f"📧 Assunto: {assunto}")
+    print(f"👤 Remetente: {remetente}")
+    print(f"📅 Recebido em: {recebido_em}")
+    print(f"⏳ Diferença em dias: {diferenca}")
+    print(f"📎 Total de anexos: {email.Attachments.Count}")
+    print("---------------------------------------------------")
+
+    print("📥 Salvando anexo na rede...")
 
     arquivo_salvo = salvar_anexo(email, DESTINO_REDE)
 
@@ -30,7 +48,7 @@ def main():
         return
 
     print(f"✅ Arquivo salvo em: {arquivo_salvo}")
-    print("🏁 Processo finalizado com sucesso.")
+    print("🏁 Processo finalizado com sucesso.\n")
 
 
 if __name__ == "__main__":
